74c86b72afbd03e9607fae63e93e27ed79a368ab
Rafael Andrade <rafaelreis2011@gmail.com>
2025-01-09 11:39:12 -0500

    Add ordenacao and fix bugs

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Pokemon } from '../../models/pokemon.model';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -12,8 +13,28 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class PokemonCardComponent {
   @Input() pokemon!: Pokemon;
+  pokemonDetails: any;
+  showDetailsFlag: boolean = false;
+
+  constructor(private pokemonService: PokemonService) {}
 
   capitalizeFirstLetter(name: string): string {
     return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  showDetails(): void {
+    if (this.showDetailsFlag) {
+      this.showDetailsFlag = false;
+      this.pokemonDetails = null;
+    } else {
+      this.pokemonService.getPokemonDetails(this.pokemon.name).subscribe((details) => {
+        this.pokemonDetails = details;
+        this.showDetailsFlag = true;
+      });
+    }
+  }
+
+  getPokemonTypes(): string {
+    return this.pokemonDetails.types.map((type: any) => type.type.name).join(', ');
   }
 }
